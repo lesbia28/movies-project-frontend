5585fbe6ae2bc77c101ab232a5c78f36b48bed57
lesbia28 <ejecutiva281284@hotmail.com>
2021-12-01 15:39:00 -0600

    first commit!

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MovieComponent,
+    MovieListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/movie/movie.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from 'src/app/model/movie.model';
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.css']
+})
+export class MovieComponent implements OnInit {
+
+  @Input('index')
+  index!: number;
+
+  @Input('movie')
+  movie!: Movie;
+
+
+  constructor() { }
+
+
+
+
+  ngOnInit(): void {
+  }
+
+}
